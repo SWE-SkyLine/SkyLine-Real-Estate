9a481c86ae823a1b1eea5ed86dbf6f364c07ace7
MohAmin22 <alsayedm12@gmail.com>
2023-11-15 01:14:43 +0200

    Backend initialize

---
+++ Backend/src/main/java/com/example/demo/SkyLineApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyLineApplication.class, args);
+	}
+
+}
