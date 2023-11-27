f1d29051c2114a8bb3fa742fc820bb52ad87e443
kimotarek <es-Karim.Ibrahim2025@alexu.edu.eg>
2023-11-27 04:47:51 +0200

    verifypage_initial

--- Frontend/sky_line/app/page_verify/page.tsx
+++ Frontend/sky_line/app/page_verify/page.tsx
@@ -27,28 +27,17 @@ export default  function Verification(){
  };
 
     return(
-    
-  <div className={style.container}>
-
-    <div className={style.left_img}></div>
-
-    <div className={style.right}>
-        <div className={style.logo}></div>
-        <div className={style.verify_form}> 
-            <span className={style.header_text}>Verify Your Email</span>
-            <div className={style.boxs}>
-              <Input_box  select_box={select_box} id={0} handleInputChange={handleInputChange}/>
-              <Input_box  select_box={select_box} id={1} handleInputChange={handleInputChange}/>
-              <Input_box  select_box={select_box} id={2} handleInputChange={handleInputChange}/>
-              <Input_box  select_box={select_box} id={3} handleInputChange={handleInputChange}/>
-              <Input_box  select_box={select_box} id={4} handleInputChange={handleInputChange}/>
-            </div>
-            <button className={style.btn_verify}>Verify</button>
-            <label className={style.lable}>Send Again</label>
+      <div className={style.main}>
+        {/* <h1>Verify your phone number</h1> */}
+        <div className={style.boxs}>
+          <Input_box  select_box={select_box} id={0} handleInputChange={handleInputChange}/>
+          <Input_box  select_box={select_box} id={1} handleInputChange={handleInputChange}/>
+          <Input_box  select_box={select_box} id={2} handleInputChange={handleInputChange}/>
+          <Input_box  select_box={select_box} id={3} handleInputChange={handleInputChange}/>
+          <Input_box  select_box={select_box} id={4} handleInputChange={handleInputChange}/>
         </div>
-         
-  </div>
-</div>
+
+      </div>
     );
 };
 
