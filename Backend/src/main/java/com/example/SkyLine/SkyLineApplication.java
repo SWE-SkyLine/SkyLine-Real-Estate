a3ef027775754c1a29bc546600b6a33049dd7a7b
MohAmin22 <alsayedm12@gmail.com>
2023-11-28 17:14:11 +0200

    creating entities and repos

--- Backend/src/main/java/com/example/SkyLine/SkyLineApplication.java
+++ Backend/src/main/java/com/example/SkyLine/SkyLineApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.SkyLine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ Backend/src/main/java/com/example/SkyLine/entity/User.java
@@ -0,0 +1,62 @@
+package com.example.SkyLine.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "account_type")
+    private String accountType;
+    @Column(name = "email")
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    @Column(name = "password")
+    private String password;
+    @Column(name = "phone_number")
+    private String PhoneNumber;
+
+}
