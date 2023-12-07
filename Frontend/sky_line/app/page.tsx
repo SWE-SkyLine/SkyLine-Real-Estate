2014825bb840349d9d0a0d646a40d904fba6e502
abdelrahman <abdelrahmanwael50@gmail.com>
2023-12-07 18:50:53 +0200

    navbar

--- Frontend/sky_line/app/page.tsx
+++ Frontend/sky_line/app/page.tsx
@@ -2,20 +2,24 @@
 import { useState } from "react";
 import { useRef } from "react";
 import style from "./page_verify/page.module.css"
+import 'bootstrap/dist/css/bootstrap.css'
 import Link from "next/link";
 import styles from './page.module.css';
 import Head from 'next/head';
+import Navbar from './navbar/navbar'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
  
 export default function Home() {
   return (
+
+
+    <>
+    <Navbar />
     <div className={styles.main}>
-    <button className={style.btn_verify}><Link href="/login">go to Login</Link></button>
-    <br />
-    {/* <button className={style.btn_verify}><Link href="/page_verify">go to verify</Link></button> */}
-    <br />
-    <button className={style.btn_verify}><Link href="/signup">go to signup</Link></button>
-    <br />
+        <h1>Welcome to Next.js</h1>
     </div>
+    </>
     )
 }
 
