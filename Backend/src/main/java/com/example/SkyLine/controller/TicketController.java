3e6a369099e6cab8da98d1cf2d2247466139a1f4
Ahmed Hesham <96313662+AH-HESHAM@users.noreply.github.com>
2023-12-08 03:55:31 +0200

    email sent

--- Backend/src/main/java/com/example/SkyLine/controller/TicketController.java
+++ Backend/src/main/java/com/example/SkyLine/controller/TicketController.java
@@ -1,5 +1,6 @@
 package com.example.SkyLine.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,17 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.SkyLine.DTO.TicketRequestDTO;
+import com.example.SkyLine.service.TicketService;
 
-@RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RestController
 @RequestMapping("/api")
 public class TicketController {
+
+    @Autowired
+    private TicketService ticketService;
+    
     @PostMapping("/ticket")
     public String getTicket(@RequestBody TicketRequestDTO ticket){
-        System.out.println(ticket.getEmail());
-        System.out.println(ticket.getCategory());
-        System.out.println(ticket.getReported());
-        System.out.println(ticket.getMessage());
+        ticketService.sendTicket(ticket);
+        System.out.println("ticket sent");
         return "Ticket is sent";
     }
 }
